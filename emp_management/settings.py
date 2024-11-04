9382779a500301718e48888f8cf3324895ff5f50
Mufeed <mufeedgvr@gmail.com>
2024-11-04 15:52:44 +0530

    final commit

--- emp_management/settings.py
+++ emp_management/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt.views',
+    'rest_framework_simplejwt',
     'employees',
 
 ]
